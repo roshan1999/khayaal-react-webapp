e0e7d0fe2f109818a54ec6b03bf0cb6e5e3498d0
shrenik99 <shrenikpokharna78@gmail.com>
2022-05-25 20:34:53 +0530

    Activity page initiative description added

---
+++ src/Pages/Activities/Activities.js
@@ -0,0 +1,9 @@
+import InitiativeDescription from "./Initiative-description/Initiative-description";
+
+const Activities = () => {
+    return (
+        <InitiativeDescription />
+    );
+}
+ 
+export default Activities;
