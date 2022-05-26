8c0d679827c6f8bdabfc395df4c9a739029f1282
manran03 <mananranka2001@gmail.com>
2022-05-26 14:47:10 +0530

    -

---
+++ src/Components/TeamsSecComponent/TeamsSecComponent.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Card from "../Card/Card";
+import DividerAndHeading from "../OurTeams/DividerAndHeading";
+
+const TeamsSecComponent = (props) => {
+
+    const sectionHeading = props.sectionHeading;
+    const [details, setDetails] = useState(props.details)
+
+    return (
+    <>
+        <DividerAndHeading sectionHeading={sectionHeading} />
+
+        <div className="cards">
+
+            {details.map(person => (
+
+                <Card name={person.name} imageurl={person.imageurl} position={person.position} quote={person.quote} />
+            ))}
+    
+        </div>
+    </>
+    
+
+       
+
+
+
+
+
+      );
+}
+ 
+export default TeamsSecComponent;
