4494998575c6320614ad9ba80e1697cd51d1552d
preethisun0801 <preethisun0801@gmail.com>
2022-06-08 14:34:38 +0530

    accomdated mobile view

--- src/Components/ParentLeader1/ParentLeader1.jsx
+++ src/Components/ParentLeader1/ParentLeader1.jsx
@@ -7,16 +7,34 @@ const ParentLeader1 = (props) => {
   const desec = props.desec;
   const imageurl = props.imageurl;
   return (
-    <div className="lpc">
-      <div className="lpcdes">
-        <LeaderDescription
-          nameText={name}
-          positionText={position}
-          descText={desec}
-        />
+    <div className="leader-desc-container">
+      <div className="image-right">
+        <div className="lpc">
+          <div className="lpcdes-1">
+            <LeaderDescription
+              nameText={name}
+              positionText={position}
+              descText={desec}
+            />
+          </div>
+          <div className="lpc-image-holder">
+            <img src={imageurl} alt="Leader's Image" />
+          </div>
+        </div>
       </div>
-      <div className="lpc-image-holder">
-        <img src={imageurl} alt="Leader's Image" />
+      <div className="image-between">
+        <div className="lpc-image-holder">
+          <img src={imageurl} alt="Leader's Image" />
+        </div>
+        <div className="lpc">
+          <div className="lpcdes">
+            <LeaderDescription
+              nameText={name}
+              positionText={position}
+              descText={desec}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
