f159dd5f61b55883cd0280e4ca1aac17d661d091
manran03 <mananranka2001@gmail.com>
2022-06-11 21:09:02 +0530

    Final-Layout

--- src/Components/ParentLeader1/ParentLeader1.jsx
+++ src/Components/ParentLeader1/ParentLeader1.jsx
@@ -1,43 +1,21 @@
 import LeaderDescription from "../OurTeams/LeaderDescription.jsx";
+import React from 'react';
 
 import "./ParentLeader1.css";
 const ParentLeader1 = (props) => {
-  const name = props.name;
-  const position = props.position;
-  const desec = props.desec;
-  const imageurl = props.imageurl;
-  return (
-    <div className="leader-desc-container">
-      <div className="image-right">
+    const name = props.name;
+    const position = props.position;
+    const desec = props.desec;
+    const imageurl = props.imageurl;
+    return (  
         <div className="lpc">
-          <div className="lpcdes-1">
-            <LeaderDescription
-              nameText={name}
-              positionText={position}
-              descText={desec}
-            />
-          </div>
-          <div className="lpc-image-holder">
-            <img src={imageurl} alt="Leader's Image" />
-          </div>
-        </div>
-      </div>
-      <div className="image-between">
-        <div className="lpc-image-holder">
-          <img src={imageurl} alt="Leader's Image" />
-        </div>
-        <div className="lpc">
-          <div className="lpcdes">
-            <LeaderDescription
-              nameText={name}
-              positionText={position}
-              descText={desec}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+            <div className="lpcdes">
+                <LeaderDescription nameText={name} positionText={position} descText={desec}/>
+            </div>
 
+            <img className="leadimage" src={imageurl} alt="Leader's Image" />
+          </div>
+    );
+};
+ 
 export default ParentLeader1;
