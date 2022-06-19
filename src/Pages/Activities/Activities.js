07617b296e5a252893db5031dbe163eef2c24c44
NameerahKazi <nameerahkazii@gmail.com>
2022-06-19 16:39:10 +0530

    Images added

--- src/Pages/Activities/Activities.js
+++ src/Pages/Activities/Activities.js
@@ -1,8 +1,10 @@
 import './Activities.css'
 import AboutActivities from './About-activities/About-activities';
-import pic1 from "../../assets/Activity-Image1.png";
+import pic1 from "../../assets/Activity-Image6.png";
 import pic2 from "../../assets/Activity-Image2.png";
 import pic3 from "../../assets/Activity-Image3.png";
+import pic4 from "../../assets/Activity-Image5.png";
+import pic5 from "../../assets/Activity-Image7.png";
 import bg from "../../assets/Activity-Page-Bg1.png";
 import bg2 from "../../assets/Activity-Page-Bg2.png";
 import ActivityComponent from './ActivityComponent/ActivityComponent';
@@ -29,14 +31,14 @@ const Activities = () => {
           <AboutActivities
             headingcolor="var(--primary-dark)" 
             contentcolor="var(--info-text-color)" 
-            pic={pic3} heading = "Donation Drive" 
+            pic={pic4} heading = "Donation Drive" 
             description = '"A piece of clothing, for peace of mind."'
             seconddescription = "We only use 40% of our wardrobe. Why not utilize the 60% for individuals who sleep unclothed? So, Team Khayaal decided to express their solidarity with those less fortunate than us by donating clothes. We successfully donated more than 4000+ clothes in places like Calcutta, South Bombay, Andheri, Mulund, Kanpur, Uttarakhand, Andheri, thereby spreading coziness with others and bringing a smile to their face."
         />
          <AboutActivities
             bg= {bg2} 
             headingcolor="white" 
-            pic={pic2} heading = "Women of Khayaal" 
+            pic={pic5} heading = "Women of Khayaal" 
             seconddescription = "Every 3 in 5 women get harassed. Each day, 92 women get raped, that is one woman in 15 minutes! Our society failed to create a safe space for women. They go through a lot each day. Sexist remarks, victim-blaming, Abuse & a lot more. This campaign was to bring attention to the same."
          />
          <AboutActivities
