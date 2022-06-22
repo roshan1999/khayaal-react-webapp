a6b50bf01727fd8bc4ccf2741f90357119cc5100
manran03 <mananranka2001@gmail.com>
2022-06-23 00:02:34 +0530

    22 june changes

--- src/Pages/OurTeams/OurTeams.jsx
+++ src/Pages/OurTeams/OurTeams.jsx
@@ -32,43 +32,43 @@ import FAB from '../Floating-Button/FAB';
 const OurTeams = () => {
     const founder = "Greetings everyone. I am currently pursuing a Bachelor's degree in Management Studies. I am a very outspoken and extroverted individual. My passion can be stationed down to helping people. It’s unbearable for me to see someone in pain and if I can help a person in need or make a stray dog wiggle its tail, my day is made. I believe we all can make an impactful change in society if our heart is full of pure intentions. At heart, I am a person who cares about the environment and children."
     const founderbold=" I strongly believe that education combined with life skills can make the world a better place for those that find it difficult to access affordable education!"
-    const  loremText="Help others without expectations, keep your intentions clear and you will be rewarded."
+    const  lovwText="Help others without expectations, keep your intentions clear and you will be rewarded."
     const Cofounder=" Being an enterprising individual who believes in creating an impact while making an income, I believe kindness is the only thing that matters when it comes to being a part of others' lives! I really couldn't care less about where someone comes from, as long as they are willing to learn new things and hold a certain sense of empathy towards others, I'll be there for them, even when they have nothing. Helping others comes naturally to me. As much as running a whole organization that helps people is a conscious thought, helping someone isn't. "
     const cofounderbold= "Khayaal is the part of my journey that allows me to help others, fuelling my forever subconscious to help others while making the life I've always dreamed for myself!"
     const educationCamp=[
-        {name:"Palak Bhutani", imageurl:ect1, position:"President", quote:loremText},
-        {name:"Khushi Mundra", imageurl:ect2, position:"Vice President", quote:loremText},
-        {name:"Shreya Tiwari", imageurl:ect3, position:"Team Leader", quote:loremText}
+        {name:"Pallak Bhutani", imageurl:ect1, position:"President", quote:lovwText},
+        {name:"Khushi Mundra", imageurl:ect2, position:"Vice President", quote:lovwText},
+        {name:"Shreya Tiwari", imageurl:ect3, position:"Team Leader", quote:lovwText}
     ];
 
     const publicRelations=[
-        {name:"Viraj Gaikwad", imageurl:pr1, position:"President", quote:loremText},
-        {name:"Dhruv Shah", imageurl:pr2, position:"Team Leader", quote:loremText}
+        {name:"Viraj Gaikwad", imageurl:pr1, position:"President", quote:lovwText},
+        {name:"Dhruv Shah", imageurl:pr2, position:"Team Leader", quote:lovwText}
     ];
 
     const stratergyTeam=[
-        {name:"Uday Somaiya", imageurl:st1, position:"President", quote:loremText}
+        {name:"Uday Somaiya", imageurl:st1, position:"President", quote:lovwText}
     ];
 
     const contentWriting=[
-        {name:"Janvi Bhanushali", imageurl:cwt1, position:"President", quote:loremText},
-        {name:"Divyanshi Chopra", imageurl:cwt2, position:"Vice President", quote:loremText},
-        {name:"Heer Danani", imageurl:cwt3, position:"Team Leader", quote:loremText},
-        {name:"Srishti Tiwari", imageurl:cwt4, position:"Team Leader", quote:loremText}
+        {name:"Janvi Bhanushali", imageurl:cwt1, position:"President", quote:lovwText},
+        {name:"Divyanshi Chopra", imageurl:cwt2, position:"Vice President", quote:lovwText},
+        {name:"Heer Danani", imageurl:cwt3, position:"Team Leader", quote:lovwText},
+        {name:"Srishti Tiwari", imageurl:cwt4, position:"Team Leader", quote:lovwText}
     ];
 
     const designTeam=[
-        {name:"Sarika Kamble", imageurl:dt1, position:"President", quote:loremText},
-        {name:"Ishika Soni", imageurl:dt2, position:"Team Leader", quote:loremText}
+        {name:"Sanika Kamble", imageurl:dt1, position:"President", quote:lovwText},
+        {name:"Ishika Soni", imageurl:dt2, position:"Team Leader", quote:lovwText}
     ];
 
     const contentCreaters=[
-        {name:"Iram Dhanani", imageurl:cc1, position:"President", quote:loremText},
-        {name:"Jay Rathod", imageurl:cc2, position:"Team Leader", quote:loremText}
+        {name:"Iram Dhanani", imageurl:cc1, position:"President", quote:lovwText},
+        {name:"Jay Rathod", imageurl:cc2, position:"Team Leader", quote:lovwText}
     ];
 
     const hr=[
-        {name:"Mohammed Rangwala", imageurl:hr1, position:"President", quote:loremText}
+        {name:"Mohammed Rangwala", imageurl:hr1, position:"President", quote:lovwText}
     ];
 
 
@@ -83,7 +83,7 @@ const OurTeams = () => {
         <div className="our-teams-container">
             <MainTitle headingText="Our Leaders"></MainTitle>
             <div id="leaders">
-            <ParentLeader1 name="Sharmishtha Singh" position="Founder of Khayaal" desec={founder} bold={founderbold} imageurl={Founder}></ParentLeader1> <br/>
+            <ParentLeader1 name="Sharmishtha Singh" position="Founder of Khayaal" desec={founder} bold={founderbold} imageurl={Founder}></ParentLeader1> 
             <ParentLeader2 name="Rohan Pariakar" position="Co-Founder of Khayaal" desec={Cofounder} bold={cofounderbold}imageurl={CO_F}></ParentLeader2>
             </div>
             <br/><br/>
