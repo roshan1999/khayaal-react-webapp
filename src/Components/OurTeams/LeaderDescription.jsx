854a484e74ea141570393827912aa05b07c296d3
preethisun0801 <preethisun0801@gmail.com>
2022-06-28 13:12:01 +0530

    our teams final adjustments

--- src/Components/OurTeams/LeaderDescription.jsx
+++ src/Components/OurTeams/LeaderDescription.jsx
@@ -1,11 +1,13 @@
 import "./LeaderDescription.css"
 import React from 'react';
+import FilledButton from "../FilledButton/FilledButton";
 const LeaderDescription = (props) => {
     return (
         <div className="leader-desc">
             <h2 className="leader-name">{props.nameText}</h2>
             <h3 className="leader-position">{props.positionText}</h3>
-           <center> <p className="leader-contents">{props.descText}<br/><b className="boldt">{props.bold}</b></p></center>
+           <center> <p className="leader-contents">{props.descText}<br/></p><b className="boldt">{props.bold}</b></center>
+           <center><button className="simple-btn">Know More</button></center>
         </div>
      );
 }
