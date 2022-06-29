6571dbb8895d787807b3d6aa1aabe745c3ec618f
NameerahKazi <nameerahkazii@gmail.com>
2022-06-29 10:43:36 +0530

    Activity page new changes

---
+++ src/Pages/Activities/Activity-sections/ActivitySections.jsx
@@ -0,0 +1,34 @@
+import './ActivitySections.css';
+
+const ActivitySections = (props) => {
+
+    return (
+       <section className='Page3Component'>
+        <div className="sectionContainer">
+            <div className="sectionContent">
+                <h1 className='sectionHeading'>Other Activities</h1>
+            </div>
+            <div className="activityCardComponent">
+                {/* <div className="activityCard">
+                    <h1 className='activityCardName1'>Covid Taskforce</h1>
+                </div>
+                <div className="activityCard">
+                    <h1 className='activityCardName2'>Donation Drive</h1>
+                </div>
+                <div className="activityCard">
+                    <h1 className='activityCardName3'>Women of Khayaal</h1>
+                </div>
+                <div className="activityCard">
+                    <h1 className='activityCardName4'>Feed the paws</h1>
+                </div> */}
+                <a className='activityCard' href="#covid">Covid Taskforce</a>
+                <a className='activityCard' href="#donation">Donation Drive</a>
+                <a className='activityCard' href="#women">Women of Khayaal</a>
+                <a className='activityCard' href="#feed">Feed the paws</a>
+            </div>
+        </div>
+       </section>
+    );
+}
+
+export default ActivitySections;
--- src/Pages/Activities/ActivityComponent/ActivityComponent.jsx
+++ src/Pages/Activities/ActivityComponent/ActivityComponent.jsx
@@ -7,14 +7,17 @@ const ActivityComponent = (props) => {
         <section className='page2Component'>
             <div className="componentContainer">
             <div className="leftComp">
-                <div className="contentComp">
-                    <h1 className='leftHeadingComp'>Activity</h1>
-                    <p className='leftContentComp'>About the Picture</p>
-                </div>
+                    <div className="rightComp">
+                        <img src={Picture3} alt="" />
+                    </div>
+                    <div className="contentComp">
+                        <h1 className='leftHeadingComp'>Education Campaign</h1>
+                        <h2 className='QuoteComp'><q>A pen, book & mentor can change the world.</q></h2>
+                        <p className='leftContentComp'>We strongly believe that education opens the doors for innumerable opportunities, but sadly not all children out there get it. To be their mentor and guide was an amazing opportunity for us because - “Jab padhega India tabhi toh badhega India.” We at Khayaal are teaching more than 75 students currently, starting from basic subjects like English and Math to important lessons such as morals and ethical values! <br/><br/><b>Team-Khayaal is putting all the effort to make their future brighter and better.</b></p>
+                    </div>
             </div>
-            <div className="rightComp">
-                <img src={Picture3} alt="" />
-            </div>
+            <div className='activities-divider'></div>
+        
         </div>
         </section>
 
