ff7865bda6422076b0c2d9d68e8950f19708d536
preethisun0801 <preethisun0801@gmail.com>
2022-06-29 21:39:14 +0530

    our teams - changed divider

--- src/Components/OurTeams/DividerAndHeading.jsx
+++ src/Components/OurTeams/DividerAndHeading.jsx
@@ -2,11 +2,11 @@ import "./DividerAndHeading.css";
 const DividerAndHeading = (props) => {
   return (
     <div className="divhead">
-      <div className="page-divider"></div>
-      <div className="section-heading">
+      {/* <div className="page-divider"></div>
+      <div className="section-heading"> */}
         <h2>{props.sectionHeading}</h2>
       </div>
-    </div>
+    // </div>
   );
 };
 
