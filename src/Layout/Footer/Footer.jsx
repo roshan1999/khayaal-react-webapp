7c62f1358c88799e3cdf634fd504ab90c9129cf1
Pramath Ramesh <54742524+pramathramesh@users.noreply.github.com>
2023-05-13 09:15:35 +0530

    Business' Address & Padding fixes (#4)
    Adding Khayaal's work address to the website along with changes to the contact information.
    Padding issues for the following were addressed:
    
    The home page and logo sections for tablet devices overlapped.
    Footer document links on mobile devices were obstructed by the phone's bottom nav gesture buttons.
    DonateBox is slightly skewed to the left when wrapped below the DonateDescription component.
    Align the footer social media logos.

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -4,6 +4,9 @@ import FbLogo from '../../assets/FooterLogo/fb.png';
 import IgLogo from '../../assets/FooterLogo/ig.png';
 import LinkedInLogo from '../../assets/FooterLogo/linkedin.png';
 import TwitterLogo from '../../assets/FooterLogo/twitter.png';
+import phoneIcon from "../../assets/FooterLogo/phone-call_filled.png"
+import emailIcon from "../../assets/FooterLogo/email_filled.png"
+import locationIcon from "../../assets/FooterLogo/location_filled.png"
 
 
 function Footer() {
@@ -12,14 +15,30 @@ function Footer() {
         termsAndConditions: "https://docs.google.com/document/d/1DilKI5Vw98dpILeGXOcADehPVceo-cj0/edit?usp=drivesdk&ouid=105560095203525235771&rtpof=true&sd=true"
     }
     const imgList = [{ img: FbLogo, link: "https://facebook.com/teamkhayaal" }, { img: IgLogo, link: "https://www.instagram.com/teamkhayaal/" }, { img: LinkedInLogo, link: "https://www.linkedin.com/company/khayaaldeliveringhope/" }, { img: TwitterLogo, link: "https://twitter.com/teamkhayaal" }];
+    const contactDetails = {
+        title: "Contact Us",
+        phone: "9161834999",
+        email: "team@khayaal.org",
+        address: "3/60, Khayaal Foundation, Gomti Nagar, Vishal Khand, Gomti Nagar Rd, Lucknow 226010"
+    }
 
     return (
         <div className="footer-main-container">
             <div className="footer-container">
                 <div className="footer-contact-container">
-                    <span>Contact Us</span>
-                    <span>9876543210</span>
-                    <span>team@khayaal.org</span>
+                    <span><b>{contactDetails.title}</b></span>
+                    <span>
+                        <img src={phoneIcon} alt='phone-icon' style={{width: "1.3rem", height: "1.3rem"}} />
+                        <span>{contactDetails.phone}</span>
+                    </span>
+                    <span>
+                        <img src={emailIcon} alt='email-icon' style={{width: "1.3rem", height: "1.3rem"}} />
+                        <span>{contactDetails.email}</span>
+                    </span>
+                    <span>
+                        <img src={locationIcon} alt='location-icon' style={{width: "1.3rem", height: "1.3rem"}} />
+                        <span>{contactDetails.address}</span>
+                    </span>
                 </div>
                 <div className="footer-logo-container">
                     {imgList.map((imgInfo, index) => {
@@ -33,7 +52,7 @@ function Footer() {
                     <p>Subscribe to our Newsletter </p>
                     <p>for updates and upcoming events</p>
                     <div className="footer-action-items">
-                        <input type="email" name="email" id="email-newsletter" />
+                        <input type="email" name="email" id="email-newsletter" placeholder='Email Address' />
                         <button>Subscribe</button>
                     </div>
                 </div>
